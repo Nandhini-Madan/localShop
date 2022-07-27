a36ce10000f5a6eb2d8cd28e0153224e64300f27
unknown <nandhinimadan20@gmail.com>
2022-07-27 11:12:27 -0700

    added seed to mycart,bills and address

--- database/seeds/00_clean.js
+++ database/seeds/00_clean.js
@@ -1,8 +1,3 @@
 exports.seed=async function(knex){
-    //await knex('billAddress').truncate()
-    //await knex('bill').truncate()
-    // await knex('myCart').truncate()
-    // await knex('products').truncate()
-    // await knex('users').truncate()
-    // await knex('roles').truncate()
+   
 }
--- database/seeds/05_bill.js
+++ database/seeds/05_bill.js
@@ -1,7 +1,7 @@
 exports.seed=async function(knex){
-    // await knex('bill').insert([
-    //     {
-            
-    //     }
-    // ])
+    await knex('bill').insert([
+        {
+            saleId: 2,customerId:3,productName:"diffuser oil",price:10.2
+        }
+    ])
 }
